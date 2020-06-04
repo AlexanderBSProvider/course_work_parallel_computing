d8da38592b72324d74d7f3455c1faac4bf317e5f
AlexanderBSProvider <shvedchenko.sasha@gmail.com>
2020-06-04 14:11:00 +0300

    Modified classes

--- course1_1/src/FindWord.java
+++ course1_1/src/FindWord.java
@@ -6,30 +6,35 @@ import java.io.File;
 
 
 class FindWord implements Runnable {
-    private Map<String, List<Integer>> MapWord ;
-    private String Word;
+    private Map<String, List<String>> MapWord ;
     private List<File> Files;
 
-    public FindWord(String Word, List<File> Files, Map<String, List<Integer>> MapWord) {
-        this.Word = Word;
+    public FindWord(List<File> Files, Map<String, List<String>> MapWord) {
         this.Files = Files;
         this.MapWord = MapWord;
     }
 
     public void run() {
         String Line;
+        List<String> GetWord;
+        List<String> words = new ArrayList<>();
         for (File File : Files) {
             String FileName = File.getName();
             try (FileReader FileReader = new FileReader(File)) {
                 BufferedReader BufferedReader = new BufferedReader(FileReader);
                 while ((Line = (BufferedReader.readLine())) != null) {
-                    List<String> words = Arrays.asList(Line.replaceAll("\\W", " ").split(" "));
-                    if (words.contains(Word)) {
-                        if (this.MapWord.containsKey(FileName) == false) {
-                            this.MapWord.put(FileName,new ArrayList<>());
+                    words = Arrays.asList(Line.replaceAll("\\W", " ").split(" "));
+                }
+                    for (String word: words) {
+                        //GetWord = this.MapWord.get(word);
+                            this.MapWord.putIfAbsent(word, new ArrayList<>());
+                            GetWord = this.MapWord.get(word);
+                            if (GetWord != null) {
+                                if (GetWord.contains(FileName) == false) {
+                                    GetWord.add(FileName);
+                                }
                         }
                     }
-                }
             } catch (IOException e) {
             }
         }
--- course1_1/src/InvertedIndex.java
+++ course1_1/src/InvertedIndex.java
@@ -1,3 +1,6 @@
+import com.sun.source.tree.Tree;
+
+import java.lang.reflect.Array;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
@@ -7,24 +10,28 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
+import java.util.Collections;
+
 
 public class InvertedIndex {
-    private static int NumberOfThreads = 4;
-    private static String[] DirName = {"D:\\Paral\\aclImdb\\test\\neg","D:\\Paral\\aclImdb\\test\\pos","D:\\Paral\\aclImdb\\train\\neg","D:\\Paral\\aclImdb\\train\\pos","D:\\Paral\\aclImdb\\train\\unsup"};
+    private static int NumberOfThreads = 32;
+    private static String[] DirName = {"D:\\Paral\\aclImdb\\test\\neg", "D:\\Paral\\aclImdb\\test\\pos","D:\\Paral\\aclImdb\\train\\neg","D:\\Paral\\aclImdb\\train\\pos","D:\\Paral\\aclImdb\\train\\unsup"};
     public static void main(String[] args)  throws IOException {
         System.out.print("Введите слово: ");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String SearchedWord   = reader.readLine();
-        Map<String, List<Integer>> MapIndex = new HashMap<>();
+        long firsttime = System.currentTimeMillis();
+        Map<String, List<String>> MapIndex = new HashMap<>();
+        Map<String, List<String>> TreeMapIndex = new TreeMap<>();
         List<Thread> threads   = new ArrayList<>();
         List<File> ListOfFiles = getPaths();
         int koef = ListOfFiles.size() / NumberOfThreads;
         for (int i = 0; i < NumberOfThreads; i++){
             Thread thread;
             if (i == NumberOfThreads- 1) {
-                thread = new Thread(new FindWord(SearchedWord, ListOfFiles.stream().skip(i*koef).collect(Collectors.toList()), MapIndex));
+                thread = new Thread(new FindWord(ListOfFiles.stream().skip(i*koef).collect(Collectors.toList()), TreeMapIndex));
             } else {
-                thread = new Thread(new FindWord(SearchedWord, ListOfFiles.stream().skip(i*koef).limit(koef).collect(Collectors.toList()), MapIndex));
+                thread = new Thread(new FindWord(ListOfFiles.stream().skip(i*koef).limit(koef).collect(Collectors.toList()), TreeMapIndex));
             }
             threads.add(thread);
             thread.start();
@@ -36,11 +43,13 @@ public class InvertedIndex {
                 System.out.println("Не удалось завершить поток");
             }
         }
-        if (MapIndex.size()>0) {
-            System.out.println("Слово найдено в файлах: " + MapIndex.keySet());
+        if (TreeMapIndex.get(SearchedWord) != null) {
+            System.out.println("Слово найдено в файлах: " + TreeMapIndex.get(SearchedWord));
         } else {
             System.out.println("Слово не найдено (╯°□°）╯︵ ┻━┻");
         }
+        System.out.println("Время выполнения: "+(System.currentTimeMillis()- firsttime));
+
     }
 
     public static List<File> getPaths() {
@@ -58,7 +67,24 @@ public class InvertedIndex {
                 lastIndex  = 250;
             }
             try {
-                temp = Files.walk(Paths.get(dir)).map(Path::toFile).skip(firstIndex).limit(lastIndex).collect(Collectors.toList());
+                temp = Files.walk(Paths.get(dir)).map(Path::toFile).sorted(new Comparator<>(){
+
+                    public int compare(File o1, File o2) {
+                        int n1 = extractNumber(o1.getName());
+                        int n2 = extractNumber(o2.getName());
+                        return n1-n2;
+                    }
+                    private int extractNumber(String name) {
+                        int i = 0;
+                        try {
+                            String[] strings = name.split("_");
+                            i = Integer.parseInt(strings[0]);
+                        } catch(Exception e) {
+                            i = 0;
+                        }
+                        return i;
+                    }
+                }).skip(firstIndex).limit(lastIndex).collect(Collectors.toList());
                 files.addAll(temp);
             } catch (IOException e) {
              System.out.println("Файл: "+dir+" не найден");
